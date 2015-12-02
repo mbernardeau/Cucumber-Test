92afa5914510c6755ac296ac04f734c3187418fb
mbernardeau <mathias.bernardeau@insa-rennes.fr>
2015-12-02 14:20:14 +0100

    Added classes and test and features

---
+++ src/com/insa/test/BookSearchTest.java
@@ -0,0 +1,12 @@
+package com.insa.test;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="Features/Scenario1.features")
+public class BookSearchTest {
+	
+}
